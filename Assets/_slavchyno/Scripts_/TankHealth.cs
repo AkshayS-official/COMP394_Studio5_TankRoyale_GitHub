b0b5e1012737e1e75d2c361267ef0d17aacf5f0f
AkshayS-official <akshayofficial0112@gmail.com>
2025-11-11 12:00:43 -0500

    Add explosive barrel and tank health scripts
    Introduced ExplosiveBarrel and TankHealth_ scripts to handle barrel explosions and tank health management. Updated the main scene to include a new wooden watch tower prefab with physics components.

---
+++ Assets/_slavchyno/Scripts_/TankHealth.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TankHealth_ : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void ApplyDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ApplyHeal(float heal)
+    {
+        health = Mathf.Min(health + heal, 100f);
+    }
+}
